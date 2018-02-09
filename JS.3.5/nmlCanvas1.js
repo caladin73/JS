b29c931d6cc4bfd8cc93d739b42d60c656b06a50
Peter <myrvig@gmail.com>
2018-02-09 07:14:50 +0100

    opgave 3.5-3.6 færdig

--- JS.3.5/nmlCanvas1.js
+++ JS.3.5/nmlCanvas1.js
@@ -14,10 +14,12 @@ let poly = function () {
         ctx.moveTo(50, 200);    // goto coordinate in canvas
         ctx.lineTo(150, 50);    // line to coordinate
         ctx.lineTo(180, 150);   // another line to coord
+        ctx.lineTo(50, 200);    // close poly
         ctx.fillStyle = 'silver';
         ctx.strokeStyle = 'black';
         ctx.lineWidth = 10;
         ctx.fill();             // fills poly
+        ctx.closePath();        // close path
         ctx.stroke();           // draws lines
     }
 }
