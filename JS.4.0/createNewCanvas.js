aa708a4ba1c5f1281cea7f968197e211251d19c9
Peter <myrvig@gmail.com>
2018-02-10 16:26:32 +0100

    op

--- JS.4.0/createNewCanvas.js
+++ JS.4.0/createNewCanvas.js
@@ -1,31 +1,44 @@
 var createNewCanvas = document.getElementById("createNewCanvas");
-var resetCreateNewCanvas = document.getElementById("resetCreateNewCanvas");
+//var resetCreateNewCanvas = document.getElementById("resetCreateNewCanvas");
 var userInputWindow = document.getElementById("userInputWindow");
 var enterCanvasHeight = document.getElementById("enterCanvasHeight");
 var enterCanvasWidth = document.getElementById("enterCanvasWidth");
 
 
 
+//if (createNewCanvas == null){
 
-function createNewCanvass() {
+    function createNewCanvass() {
+        //document.body.removeChild(userInputWindow);
 
-    document.body.removeChild(userInputWindow);
-
-    var enterCanvasHeightt = enterCanvasHeight.value;
-    var enterCanvasWidthh = enterCanvasWidth.value;
-    document.body.innerHTML = enterCanvasHeightt+":"+enterCanvasWidthh;
-
-    var newCanvas = document.createElement("canvas");
-
-    newCanvas.classList.add("canvasDesign");
-
-    newCanvas.style.height = enterCanvasHeightt+"px";
-    newCanvas.style.width = enterCanvasWidthh+"px";
+        var enterCanvasHeightt = enterCanvasHeight.value;
+        var enterCanvasWidthh = enterCanvasWidth.value;
+        var newCanvas = document.createElement("canvas");
 
 
-    document.body.appendChild(newCanvas);
+        newCanvas.classList.add("canvasDesign");
+        newCanvas.style.height = enterCanvasHeightt+"px";
+        newCanvas.style.width = enterCanvasWidthh+"px";
+        document.body.appendChild(newCanvas);
+
+        //document.body.innerHTML = enterCanvasHeightt+":"+enterCanvasWidthh;
+
+        var toolboxCanvas = document.createElement("canvas");
+        toolboxCanvas.classList.add("canvasDesign");
+        document.body.appendChild(toolboxCanvas);
+        document.getElementById("createNewCanvas").disabled = true;
 
 
-}
+
+
+
+
+
+
+
+
+
+    }
 
 createNewCanvas.addEventListener('click', createNewCanvass);
+
