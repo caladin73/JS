c3b6ea490bc448819a01f158625fcb7d500bbcd6
Peter <myrvig@gmail.com>
2018-02-12 14:08:47 +0100

    op

--- JS.4.0/adactio110.js
+++ JS.4.0/adactio110.js
@@ -7,12 +7,6 @@
 
 let create_canvas = function () {
 
-//let initialize = function () {
-
-    var i = document.getElementById("room").style.width;
-
-    if (i == 0) {
-
         var x = document.getElementById("myForm").elements[0].value;
         var y = document.getElementById("myForm").elements[1].value;
 
@@ -22,12 +16,9 @@ let create_canvas = function () {
         document.getElementById("toolbox").style.width = 400 + "px";
         document.getElementById("toolbox").style.height = 400 + "px";
 
-    }
 
-    let initialize = function () {
 
-        let mycv = Object.create(Canvas);
-        mycv.init('myCanvas', 'transparent');
+        mycv.init('toolbox');
         mycv.canvas.addEventListener('click', hittest);
 
         let shape1 = Object.create(Shape);
@@ -36,38 +27,14 @@ let create_canvas = function () {
         shapes.push(shape1);
         repeater(mycv, shapes);
 
+        let shapes = [];
 
-        
+        window.addEventListener('load', initialize);
 
-        /*
-        let canvas = $('toolbox');
-        var ctx = canvas.getContext('2d');
 
-        ctx.fillStyle = "blue";         // fill color to 088
-        ctx.fillRect(20, 10, 100, 100);  // fill rectangle
 
-        ctx.fillStyle = "red";         // fill color to 088
-        ctx.fillRect(200, 10, 80, 80);  // fill rectangle
 
-        ctx.fillStyle = "green";         // fill color to 088
-        ctx.fillRect(20, 150, 60, 60);  // fill rectangle
 
-        ctx.fillStyle = "pink";         // fill color to 088
-        ctx.fillRect(200, 150, 40, 40);  // fill rectangle
-
-        ctx.beginPath();                // begin new path
-        ctx.arc(80, 300, 50, 0, Math.PI, true);
-        ctx.fillStyle = "yellow";         // set fill color
-        ctx.fill();                     // fill the path
-        ctx.stroke();                   // draw circumference
-
-        */
-
-    }
-
-    let shapes = [];
-
-    window.addEventListener('load', initialize);
 
 };
 
