92e15a21da38a8d7f4beff71541653aef3ed3585
Peter <myrvig@gmail.com>
2018-02-12 13:50:36 +0100

    op

--- JS.4.0/adactio110.js
+++ JS.4.0/adactio110.js
@@ -5,13 +5,13 @@
  * adactio110.js
  */
 
-//let create_canvas = function () {
+let create_canvas = function () {
 
-let initialize = function () {
+//let initialize = function () {
 
-    //var i = document.getElementById("room").style.width;
+    var i = document.getElementById("room").style.width;
 
-    //if (i == 0) {
+    if (i == 0) {
 
         var x = document.getElementById("myForm").elements[0].value;
         var y = document.getElementById("myForm").elements[1].value;
@@ -21,47 +21,54 @@ let initialize = function () {
 
         document.getElementById("toolbox").style.width = 400 + "px";
         document.getElementById("toolbox").style.height = 400 + "px";
-    
+
+    }
+
+    let initialize = function () {
+
+        let mycv = Object.create(Canvas);
+        mycv.init('myCanvas', 'transparent');
+        mycv.canvas.addEventListener('click', hittest);
+
+        let shape1 = Object.create(Shape);
+        shape1.init(mycv, 20, 10, 120, 40, 'blue');
+
+        shapes.push(shape1);
+        repeater(mycv, shapes);
 
 
+        
 
-    let mycv = Object.create(Canvas);
-    mycv.init('myCanvas', 'transparent');
-    mycv.canvas.addEventListener('click', hittest);
+        /*
+        let canvas = $('toolbox');
+        var ctx = canvas.getContext('2d');
 
-    let shape1 = Object.create(Shape);
-    shape1.init(mycv, 20, 10, 120, 40, 'blue');
+        ctx.fillStyle = "blue";         // fill color to 088
+        ctx.fillRect(20, 10, 100, 100);  // fill rectangle
 
-    shapes.push(shape1);
-    repeater(mycv, shapes);
+        ctx.fillStyle = "red";         // fill color to 088
+        ctx.fillRect(200, 10, 80, 80);  // fill rectangle
 
+        ctx.fillStyle = "green";         // fill color to 088
+        ctx.fillRect(20, 150, 60, 60);  // fill rectangle
 
+        ctx.fillStyle = "pink";         // fill color to 088
+        ctx.fillRect(200, 150, 40, 40);  // fill rectangle
 
-    /*
-    let canvas = $('toolbox');
-    var ctx = canvas.getContext('2d');
+        ctx.beginPath();                // begin new path
+        ctx.arc(80, 300, 50, 0, Math.PI, true);
+        ctx.fillStyle = "yellow";         // set fill color
+        ctx.fill();                     // fill the path
+        ctx.stroke();                   // draw circumference
 
-    ctx.fillStyle = "blue";         // fill color to 088
-    ctx.fillRect(20, 10, 100, 100);  // fill rectangle
+        */
 
-    ctx.fillStyle = "red";         // fill color to 088
-    ctx.fillRect(200, 10, 80, 80);  // fill rectangle
+    }
 
-    ctx.fillStyle = "green";         // fill color to 088
-    ctx.fillRect(20, 150, 60, 60);  // fill rectangle
+    let shapes = [];
 
-    ctx.fillStyle = "pink";         // fill color to 088
-    ctx.fillRect(200, 150, 40, 40);  // fill rectangle
+    window.addEventListener('load', initialize);
 
-    ctx.beginPath();                // begin new path
-    ctx.arc(80, 300, 50, 0, Math.PI, true);
-    ctx.fillStyle = "yellow";         // set fill color
-    ctx.fill();                     // fill the path
-    ctx.stroke();                   // draw circumference
-
-    */
 };
 
-let shapes = [];
 
-window.addEventListener('load', initialize);
