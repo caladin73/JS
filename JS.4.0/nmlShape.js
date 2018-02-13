d4eccc68e19375a06f9ea00ac45597d5f80e751a
Peter Myrvig <myrvig@gmail.com>
2018-02-13 10:46:08 +0100

    op

--- JS.4.0/nmlShape.js
+++ JS.4.0/nmlShape.js
@@ -4,28 +4,68 @@
 /**
  * Shape object
  */
+
 let Shape = {
-    init(cv, x, y, r, width, height, color) {
+    init(cv, x, y, color) {
         this.ctx = cv.context;
         this.x = x;
         this.y = y;
-        this.r = r;
-        this.width = width;
-        this.height = height;
         this.color = color;
     },
+}
 
-    draw() {
-        this.ctx.fillStyle = this.color;
-        this.ctx.fillRect(this.x, this.y, this.width, this.height);
-        this.ctx.beginPath();
-        this.ctx.arc(this.x, this.y, this.r, 0, Math.PI, false);
-        this.ctx.fillStyle = this.color;
-        this.ctx.fill();
-    },
+let Rect = {
+    width: null;
+height: null;
 
-    move(dx, dy) {
-        this.x += dx;
-        this.y += dy;
-    }
+setup(cv, x, y, color)
+{
+    this.init(cv, x, y, color)
+    this.width = width;
+    this.height = height;
+}
+,
+
+getWidth()
+{
+    return this.width;
+}
+getHeight()
+{
+    return this.height;
+}
+
+}
+
+
+let Arc = {
+
+    r: null;
+
+setup(cv, x, y, color) {
+    this.init(cv, x, y, color)
+    this.r = return;
+}
+
+getR() {
+    return this.r;
+}
+
+
+drawRect() {
+    this.ctx.fillStyle = this.color;
+    this.ctx.fillRect(this.x, this.y, this.width, this.height)
+}
+
+drawArc() {
+    this.ctx.beginPath();
+    this.ctx.arc(this.x, this.y, this.r, 0, Math.PI, false);
+    this.ctx.fillStyle = this.color;
+    this.ctx.fill();
+},
+
+move(dx, dy) {
+    this.x += dx;
+    this.y += dy;
+}
 };
